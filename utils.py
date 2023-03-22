e20a091ce1ca128e34dee64ad476e7dec2d415ed
André Moreira Souza <msouza.andre@hotmail.com>
2023-03-22 14:05:42 -0300

    Rework metrics for train and test functions

--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ def train(
     num_epochs: int,
     n_classes: int = 1,
     verbose: bool = True,
-) -> None:
+) -> dict:
     """Train a PyTorch model.
 
     Args:
@@ -36,26 +36,32 @@ def train(
         num_epochs (int): number of epochs
 
     Returns:
-        None
+        dict: dictionary of metrics
     """
     model.train()  # set model to training mode
-
+    metrics: dict = {
+        "loss": [],
+        "accuracy": [],
+        "hamming": [],
+        "f1-score": [],
+        "roc_auc": [],
+    }
     # loop over epochs
     for epoch in range(num_epochs):
         running_loss: float = 0.0
         running_corrects: int = 0
-
         # initialize inputs and labels tensors
         inputs: torch.Tensor = torch.empty(0)
         labels: torch.Tensor = torch.empty(0)
+        # Acumulate labels and predictions for F1 score
+        result_labels: torch.Tensor = torch.empty(0)
+        result_preds: torch.Tensor = torch.empty(0)
         # loop over data
         for inputs, labels in train_loader:
             inputs = inputs.to(device)
             labels = labels.to(device)
-
             # zero the parameter gradients
             optimizer.zero_grad()
-
             # forward
             # track history if only in train
             with torch.set_grad_enabled(True):
@@ -64,9 +70,8 @@ def train(
                 # get probability for each class based on threshold
                 # if probability >= threshold, then class = 1, else class = 0
                 preds = (outputs >= config.PRED_THRESHOLD).float()
-
+                # calculate loss with predictions and labels
                 loss = criterion(outputs, labels)
-
                 # backward + optimize
                 loss.backward()
                 optimizer.step()
@@ -76,23 +81,49 @@ def train(
             running_corrects += int(
                 torch.sum((outputs >= config.PRED_THRESHOLD).float() == labels.data)
             )
+            result_labels = torch.cat((result_labels, labels.cpu()), dim=0)
+            result_preds = torch.cat((result_preds, preds.cpu()), dim=0)
             # running_corrects += int(torch.sum(preds == labels.data))
-
+        # Calculate epoch metrics
         epoch_loss: float = running_loss / float(len(train_loader.dataset))  # type: ignore
-        epoch_acc: float = float(running_corrects) / float(len(train_loader.dataset) * n_classes)  # type: ignore # noqa: E501 # pylint: disable=line-too-long
-        # TODO: Fix epoch_f1
-        epoch_f1: float = sklearn.metrics.f1_score(
-            y_true=labels.cpu().numpy(),
-            y_pred=preds.cpu().numpy(),
-            average="weighted",
-            zero_division=1,
+        epoch_accuracy = sklearn.metrics.accuracy_score(
+            y_true=result_labels.cpu().numpy().astype(bool),
+            y_pred=(result_preds.cpu().numpy() >= config.PRED_THRESHOLD).astype(bool),
         )
-
+        epoch_hamming = sklearn.metrics.hamming_loss(
+            y_true=result_labels.cpu().numpy().astype(int),
+            y_pred=(result_preds.cpu().numpy() >= config.PRED_THRESHOLD).astype(int),
+        )
+        epoch_f1: float = sklearn.metrics.f1_score(
+            y_true=result_labels.cpu().numpy().astype(int),
+            y_pred=(result_preds.cpu().numpy() >= config.PRED_THRESHOLD).astype(int),
+            average="weighted",
+            zero_division="warn",
+        )
+        epoch_roc_auc = sklearn.metrics.roc_auc_score(
+            y_true=result_labels.cpu().numpy().astype(int),
+            y_score=result_preds.cpu().numpy(),
+            average="weighted",
+            multi_class="ovr",
+        )
+        # Append epoch metrics
+        metrics["loss"].append(epoch_loss)
+        metrics["accuracy"].append(epoch_accuracy)
+        metrics["hamming"].append(epoch_hamming)
+        metrics["f1-score"].append(epoch_f1)
+        metrics["roc_auc"].append(epoch_roc_auc)
+        # Print epoch metrics
         if verbose:
             print(f"Epoch {epoch + 1}/{num_epochs}")
             print(
-                f"Training Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} F1: {epoch_f1:.4f}"
+                f"Training Loss: "
+                f"{epoch_loss:.4f} "
+                f"Accuracy: {epoch_accuracy:.4f} "
+                f"Hamming: {epoch_hamming:.4f} "
+                f"F1: {epoch_f1:.4f}"
+                f"ROC AUC: {epoch_roc_auc:.4f}"
             )
+    return metrics
 
 
 def test(
@@ -101,7 +132,7 @@ def test(
     criterion: torch.nn.Module,
     device: torch.device,
     verbose: bool = True,
-) -> tuple[float, float, float]:
+) -> dict:
     """Test a PyTorch model.
 
     Args:
@@ -111,15 +142,17 @@ def test(
         device (torch.device): device to test on
 
     Returns:
-        float: loss
-        float: accuracy
-        float: f1 score
+        dict: dictionary of metrics
     """
     model.eval()  # set model to evaluation mode
 
     # initialize inputs and labels tensors
     inputs: torch.Tensor = torch.empty(0)
     labels: torch.Tensor = torch.empty(0)
+
+    # Acumulate labels and predictions for F1 score
+    result_labels: torch.Tensor = torch.empty(0)
+    result_preds: torch.Tensor = torch.empty(0)
 
     # initialize loss and accuracy
     running_loss: float = 0.0
@@ -139,18 +172,50 @@ def test(
 
         # statistics
         running_loss += loss.item() * inputs.size(0)
-        running_corrects += int(torch.sum(preds == labels.data))
-
+        running_corrects += int(
+            torch.sum((outputs >= config.PRED_THRESHOLD).float() == labels.data)
+        )
+        result_labels = torch.cat((result_labels, labels.cpu()), dim=0)
+        result_preds = torch.cat((result_preds, preds.cpu()), dim=0)
+    # Calculate epoch metrics
     epoch_loss: float = running_loss / float(len(test_loader.dataset))  # type: ignore
-    epoch_acc: float = float(running_corrects) / float(len(test_loader.dataset))  # type: ignore
+    epoch_accuracy = sklearn.metrics.accuracy_score(
+        y_true=result_labels.cpu().numpy().astype(bool),
+        y_pred=(result_preds.cpu().numpy() >= config.PRED_THRESHOLD).astype(bool),
+    )
+    epoch_hamming = sklearn.metrics.hamming_loss(
+        y_true=result_labels.cpu().numpy().astype(int),
+        y_pred=(result_preds.cpu().numpy() >= config.PRED_THRESHOLD).astype(int),
+    )
     epoch_f1: float = sklearn.metrics.f1_score(
-        y_true=labels.cpu().numpy(), y_pred=preds.cpu().numpy(), average="weighted"
+        y_true=result_labels.cpu().numpy().astype(int),
+        y_pred=(result_preds.cpu().numpy() >= config.PRED_THRESHOLD).astype(int),
+        average="weighted",
+        zero_division="warn",
+    )
+    epoch_roc_auc = sklearn.metrics.roc_auc_score(
+        y_true=result_labels.cpu().numpy().astype(int),
+        y_score=result_preds.cpu().numpy(),
+        average="weighted",
+        multi_class="ovr",
     )
 
     if verbose:
-        print(f"Test Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f} F1: {epoch_f1:.4f}")
+        print(
+            f"Test Loss: {epoch_loss:.4f} "
+            f"Accuracy: {epoch_accuracy:.4f} "
+            f"Hamming: {epoch_hamming:.4f} "
+            f"F1: {epoch_f1:.4f}"
+            f"ROC AUC: {epoch_roc_auc:.4f}"
+        )
 
-    return epoch_loss, epoch_acc, epoch_f1
+    return {
+        "loss": epoch_loss,
+        "accuracy": epoch_accuracy,
+        "hamming_loss": epoch_hamming,
+        "f1-score": epoch_f1,
+        "roc_auc": epoch_roc_auc,
+    }
 
 
 def validate(
