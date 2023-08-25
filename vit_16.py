f6344b9b9286bdf52d319c743441e032ed8928d5
André Moreira Souza <msouza.andre@hotmail.com>
2023-08-25 11:38:20 -0300

    Refactor vit.py into 16x16 and 32x32 models

--- vit_16.py
+++ vit_16.py
@@ -3,6 +3,7 @@
 # # Imports
 
 # %%
+import os
 from typing import Any
 
 import lightning.pytorch as pl
@@ -23,14 +24,8 @@ from torchmetrics.classification import (
 from torchvision.models import (
     vit_b_16,
     ViT_B_16_Weights,
-    vit_b_32,
-    ViT_B_32_Weights,
     vit_l_16,
     ViT_L_16_Weights,
-    vit_l_32,
-    ViT_L_32_Weights,
-    vit_h_14,
-    ViT_H_14_Weights,
 )
 
 import config
@@ -44,48 +39,41 @@ from data import AudioDataset
 torch.set_float32_matmul_precision("medium")
 RANDOM_SEED: int = 42
 
+LEARNING_RATE: float = 1e-3
+WEIGHT_DECAY: float = 3e-2
+EXPERIMENT_NAME: str = "vit_b_16"
+
 # %% [markdown]
 # # Classes
 
 # %%
 
 
-class ViT(pl.LightningModule):
+class ViT16(pl.LightningModule):
     """ViT model for keypoint detection."""
 
     def __init__(
         self,
         num_classes: int,
         dropout: float = 0.0,
-        weights: ViT_B_16_Weights
-        | ViT_B_32_Weights
-        | ViT_L_16_Weights
-        | ViT_L_32_Weights
-        | ViT_H_14_Weights = None,
+        weights: ViT_B_16_Weights | ViT_L_16_Weights = None,
     ) -> None:
         """Initialize model.
 
         Args:
             num_keypoints (int): Number of keypoints to detect.
             dropout (float, optional): Dropout rate. Defaults to 0.0.
-            weights (
-                ViT_B_16_Weights |
-                ViT_B_32_Weights |
-                ViT_L_16_Weights |
-                ViT_L_32_Weights |
-                ViT_H_14_Weights, optional
-            ) : Weights to load. Defaults to None.
+            weights (ViT_B_16_Weights | ViT_L_16_Weights, optional): Weights to use for
+                the model. Defaults to None.
 
         Raises:
             ValueError: If weights is not None and not one of the supported weight types.
         """
         super().__init__()
         self.num_classes = num_classes
+        self.image_size = (96, 64)
         # Load pretrained model based on weights
-        # If weights is None, use default weights with vit_b_16
-        if weights is None:
-            self.model = vit_b_16(dropout=dropout, num_classes=num_classes)
-        elif isinstance(weights, ViT_B_16_Weights) or weights is None:
+        if isinstance(weights, ViT_B_16_Weights) or weights is None:
             # If weights is None, use default weights with vit_b_16
             self.model = vit_b_16(weights=weights, dropout=dropout)
             self.transforms = (
@@ -93,23 +81,17 @@ class ViT(pl.LightningModule):
                 if weights is not None
                 else ViT_B_16_Weights.DEFAULT.transforms(antialias=True)
             )
-        elif isinstance(weights, ViT_B_32_Weights):
-            self.model = vit_b_32(weights=weights, dropout=dropout)
-            self.transforms = weights.transforms(antialias=True)
-
         elif isinstance(weights, ViT_L_16_Weights):
             self.model = vit_l_16(weights=weights, dropout=dropout)
-            self.transforms = weights.transforms(antialias=True)
-        elif isinstance(weights, ViT_L_32_Weights):
-            self.model = vit_l_32(weights=weights, dropout=dropout)
-            self.transforms = weights.transforms(antialias=True)
-        elif isinstance(weights, ViT_H_14_Weights):
-            self.model = vit_h_14(weights=weights, dropout=dropout)
             self.transforms = weights.transforms(antialias=True)
         else:
             raise ValueError(f"Unknown weights: {weights}")
         # Update model heads to match number of classes
         self._update_heads()
+        # Update conv_proj to match input dimensions
+        self._update_conv_proj()
+        # Update pos_embedding to match input dimensions
+        self._update_pos_embedding()
         # Initialize training metrics
         self.train_acc = MultilabelAccuracy(
             num_labels=self.num_classes, threshold=0.5, average="weighted"
@@ -177,6 +159,46 @@ class ViT(pl.LightningModule):
         # replace the pre-trained head with a new one
         self.model.heads[-1] = nn.Linear(in_features, self.num_classes)
 
+    def _update_conv_proj(self) -> None:
+        self.model.conv_proj = nn.Conv2d(
+            in_channels=1,
+            out_channels=self.model.hidden_dim,
+            kernel_size=self.model.patch_size,
+            stride=self.model.patch_size,
+        )
+
+    def _update_pos_embedding(self) -> None:
+        self.model.encoder.pos_embedding = nn.Parameter(
+            torch.empty(1, 25, self.model.hidden_dim).normal_(std=0.02)
+        )
+
+    def _process_input(self, x: torch.Tensor) -> torch.Tensor:
+        n, c, h, w = x.shape  # pylint: disable=invalid-name
+        p = self.model.patch_size  # pylint: disable=invalid-name
+        torch._assert(
+            h == self.image_size[0],
+            f"Wrong image height! Expected {self.image_size} but got {h}!",
+        )
+        torch._assert(
+            w == self.image_size[1],
+            f"Wrong image width! Expected {self.image_size} but got {w}!",
+        )
+        n_h = h // p
+        n_w = w // p
+
+        # (n, c, h, w) -> (n, hidden_dim, n_h, n_w)
+        x = self.model.conv_proj(x)
+        # (n, hidden_dim, n_h, n_w) -> (n, n_h * n_w, hidden_dim)
+        x = x.reshape(n, self.model.hidden_dim, n_h * n_w)
+
+        # (n, hidden_dim, (n_h * n_w)) -> (n, (n_h * n_w), hidden_dim)
+        # The self attention layer expects inputs in the format (N, S, E)
+        # where S is the source sequence length, N is the batch size, E is the
+        # embedding dimension
+        x = x.permute(0, 2, 1)
+
+        return x
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass.
 
@@ -186,7 +208,22 @@ class ViT(pl.LightningModule):
         Returns:
             torch.Tensor: Predicted keypoints.
         """
-        return self.model(x)
+        # Reshape and permute the input tensor
+        x = self._process_input(x)
+        n = x.shape[0]
+
+        # Expand the class token to the full batch
+        batch_class_token = self.model.class_token.expand(n, -1, -1)
+        x = torch.cat([batch_class_token, x], dim=1)
+
+        x = self.model.encoder(x)
+
+        # Classifier "token" as used by standard language architectures
+        x = x[:, 0]
+
+        x = self.model.heads(x)
+
+        return x
 
     def training_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
         """Training step. Returns loss.
@@ -205,13 +242,13 @@ class ViT(pl.LightningModule):
         # log loss
         self.log("train_loss", loss, on_step=False, on_epoch=True)
         # calculate metrics
-        self.train_acc(F.sigmoid(outputs), targets)
-        self.train_auroc(F.sigmoid(outputs), targets)
-        self.train_f1(F.sigmoid(outputs), targets)
-        self.train_hamming(F.sigmoid(outputs), targets)
-        self.train_precision(F.sigmoid(outputs), targets)
-        self.train_recall(F.sigmoid(outputs), targets)
-        self.train_roc(F.sigmoid(outputs), targets)
+        self.train_acc(F.sigmoid(outputs), targets.int())
+        self.train_auroc(F.sigmoid(outputs), targets.int())
+        self.train_f1(F.sigmoid(outputs), targets.int())
+        self.train_hamming(F.sigmoid(outputs), targets.int())
+        self.train_precision(F.sigmoid(outputs), targets.int())
+        self.train_recall(F.sigmoid(outputs), targets.int())
+        # self.train_roc(F.sigmoid(outputs), targets.int())
         # log metrics
         self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
         self.log("train_auroc", self.train_auroc, on_step=False, on_epoch=True)
@@ -219,7 +256,7 @@ class ViT(pl.LightningModule):
         self.log("train_hamming", self.train_hamming, on_step=False, on_epoch=True)
         self.log("train_precision", self.train_precision, on_step=False, on_epoch=True)
         self.log("train_recall", self.train_recall, on_step=False, on_epoch=True)
-        self.log("train_roc", self.train_roc, on_step=False, on_epoch=True)
+        # self.log("train_roc", self.train_roc, on_step=False, on_epoch=True)
         # return loss
         return loss
 
@@ -242,13 +279,13 @@ class ViT(pl.LightningModule):
         # log loss
         self.log("val_loss", loss, on_step=False, on_epoch=True)
         # calculate metrics
-        self.val_acc(F.sigmoid(outputs), targets)
-        self.val_auroc(F.sigmoid(outputs), targets)
-        self.val_f1(F.sigmoid(outputs), targets)
-        self.val_hamming(F.sigmoid(outputs), targets)
-        self.val_precision(F.sigmoid(outputs), targets)
-        self.val_recall(F.sigmoid(outputs), targets)
-        self.val_roc(F.sigmoid(outputs), targets)
+        self.val_acc(F.sigmoid(outputs), targets.int())
+        self.val_auroc(F.sigmoid(outputs), targets.int())
+        self.val_f1(F.sigmoid(outputs), targets.int())
+        self.val_hamming(F.sigmoid(outputs), targets.int())
+        self.val_precision(F.sigmoid(outputs), targets.int())
+        self.val_recall(F.sigmoid(outputs), targets.int())
+        # self.val_roc(F.sigmoid(outputs), targets.int())
         # log metrics
         self.log("val_acc", self.val_acc, on_step=False, on_epoch=True)
         self.log("val_auroc", self.val_auroc, on_step=False, on_epoch=True)
@@ -256,7 +293,7 @@ class ViT(pl.LightningModule):
         self.log("val_hamming", self.val_hamming, on_step=False, on_epoch=True)
         self.log("val_precision", self.val_precision, on_step=False, on_epoch=True)
         self.log("val_recall", self.val_recall, on_step=False, on_epoch=True)
-        self.log("val_roc", self.val_roc, on_step=False, on_epoch=True)
+        # self.log("val_roc", self.val_roc, on_step=False, on_epoch=True)
         # return loss
         return loss
 
@@ -279,13 +316,13 @@ class ViT(pl.LightningModule):
         # log loss
         self.log("test_loss", loss)
         # calculate metrics
-        self.test_acc(F.sigmoid(outputs), targets)
-        self.test_auroc(F.sigmoid(outputs), targets)
-        self.test_f1(F.sigmoid(outputs), targets)
-        self.test_hamming(F.sigmoid(outputs), targets)
-        self.test_precision(F.sigmoid(outputs), targets)
-        self.test_recall(F.sigmoid(outputs), targets)
-        self.test_roc(F.sigmoid(outputs), targets)
+        self.test_acc(F.sigmoid(outputs), targets.int())
+        self.test_auroc(F.sigmoid(outputs), targets.int())
+        self.test_f1(F.sigmoid(outputs), targets.int())
+        self.test_hamming(F.sigmoid(outputs), targets.int())
+        self.test_precision(F.sigmoid(outputs), targets.int())
+        self.test_recall(F.sigmoid(outputs), targets.int())
+        # self.test_roc(F.sigmoid(outputs), targets.int())
         # log metrics
         self.log("test_acc", self.test_acc)
         self.log("test_auroc", self.test_auroc)
@@ -293,7 +330,7 @@ class ViT(pl.LightningModule):
         self.log("test_hamming", self.test_hamming)
         self.log("test_precision", self.test_precision)
         self.log("test_recall", self.test_recall)
-        self.log("test_roc", self.test_roc)
+        # self.log("test_roc", self.test_roc)
         # return loss
         return loss
 
@@ -313,8 +350,28 @@ class ViT(pl.LightningModule):
         outputs = self(images)
         return outputs
 
-    def configure_optimizers(self) -> torch.optim.Optimizer:
-        return torch.optim.AdamW(self.parameters(), lr=1e-3, betas=(0.9, 0.999))
+    def configure_optimizers(self) -> Any:
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
+            lr=LEARNING_RATE,
+            betas=(0.9, 0.999),
+            weight_decay=WEIGHT_DECAY,
+        )
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="max",
+            factor=0.1,
+            patience=config.EARLY_STOPPING_PATIENCE // 2,
+            threshold=0.0001,
+            threshold_mode="rel",
+            cooldown=0,
+            min_lr=0,
+            eps=1e-08,
+            verbose=True,
+        )
+        return [optimizer], [
+            {"scheduler": scheduler, "interval": "epoch", "monitor": "val_loss"}
+        ]
 
 
 # %% [markdown]
@@ -335,37 +392,70 @@ if __name__ == "__main__":
     train_annotation.to_csv(config.TRAIN_ANNOTATION_FILE, index=False)
     val_annotation.to_csv(config.VAL_ANNOTATION_FILE, index=False)
     # Create model
-    model = ViT(
-        num_classes=config.NUM_CLASSES,
-        dropout=0.0,
-    )
+    # Search for checkpoint file in models directory
+    # file starts with experiment name
+    # file ends with .ckpt
+    checkpoint_file: str | None = None
+    for file in os.listdir(config.MODELS_DIRECTORY):
+        if file.startswith(EXPERIMENT_NAME) and file.endswith(".ckpt"):
+            checkpoint_file = os.path.join(config.MODELS_DIRECTORY, file)
+            break
+    if checkpoint_file is not None:
+        model = ViT16.load_from_checkpoint(
+            checkpoint_file, num_classes=config.NUM_CLASSES, dropout=0.5
+        )
+    else:
+        model = ViT16(
+            num_classes=config.NUM_CLASSES,
+            dropout=0.5,
+        )
     # Create datasets
     train_dataset = AudioDataset(
         config.TRAIN_ANNOTATION_FILE,
         config.DATA_DIRECTORY,
         transform=lambda x: x[None, :, :64],
-        target_transform=lambda x: x[0, :],
+        target_transform=lambda x: x[0, :].float(),
     )
     val_dataset = AudioDataset(
         config.VAL_ANNOTATION_FILE,
         config.DATA_DIRECTORY,
         transform=lambda x: x[None, :, :64],
-        target_transform=lambda x: x[0, :],
+        target_transform=lambda x: x[0, :].float(),
     )
     # Create dataloaders
     train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=config.BATCH_SIZE, shuffle=True, num_workers=24
+        train_dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=True,
+        num_workers=config.NUM_WORKERS,
     )
     val_dataloader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=config.BATCH_SIZE, shuffle=False, num_workers=24
+        val_dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=False,
+        num_workers=config.NUM_WORKERS,
     )
-    early_stopping = pl.callbacks.EarlyStopping(monitor="val_loss", patience=10)
+    early_stopping = pl.callbacks.EarlyStopping(
+        monitor="val_loss", patience=config.EARLY_STOPPING_PATIENCE
+    )
     loggers = [
-        pl.loggers.CSVLogger(config.LOG_DIRECTORY, name="vit_b_16_test"),
-        pl.loggers.TensorBoardLogger(config.LOG_DIRECTORY, name="vit_b_16_test"),
+        pl.loggers.CSVLogger(config.LOG_DIRECTORY, name=EXPERIMENT_NAME),
+        pl.loggers.TensorBoardLogger(config.LOG_DIRECTORY, name=EXPERIMENT_NAME),
     ]
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=config.MODELS_DIRECTORY,
+        filename=EXPERIMENT_NAME + "-{epoch:02d}-{val_loss:.2f}-{val_auroc:.2f}",
+        monitor="val_auroc",
+        verbose=True,
+        save_top_k=1,
+        save_weights_only=False,
+        mode="max",
+        auto_insert_metric_name=True,
+        every_n_epochs=1,
+        save_on_train_epoch_end=False,
+    )
     trainer = pl.Trainer(
-        callbacks=[early_stopping],
+        callbacks=[early_stopping, checkpoint_callback],
         max_epochs=1000,
         logger=loggers,
         log_every_n_steps=min(50, len(train_dataloader)),
