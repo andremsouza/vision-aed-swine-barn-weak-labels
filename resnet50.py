54e6c7182dcbe300410c47c54b34a38a90e1d39a
André Moreira Souza <msouza.andre@hotmail.com>
2023-09-14 11:11:33 -0300

    Convert Inception, ResNet, VGG to Lightning

--- resnet50.py
+++ resnet50.py
@@ -7,15 +7,39 @@ from https://arxiv.org/abs/1512.03385. It is adapted from PyTorch
 # # Imports
 
 # %%
+import os
+from typing import Any
+
+import lightning.pytorch as pl
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torchmetrics.classification import (
+    MultilabelAccuracy,
+    MultilabelAUROC,
+    MultilabelF1Score,
+    MultilabelHammingDistance,
+    MultilabelPrecision,
+    MultilabelRecall,
+    MultilabelROC,
+)
 import torchvision
+
+import config
+from data import AudioDataset
 
 # %% [markdown]
 # # Constants
 
 # %%
-LEARNING_RATES = [0.001, 0.0001, 0.00001]
+torch.set_float32_matmul_precision("high")
+RANDOM_SEED: int = 42
+
+LEARNING_RATE: float = 1e-3
+WEIGHT_DECAY: float = 1e-2
+EXPERIMENT_NAME: str = "resnet50"
 
 # %% [markdown]
 # # Classes
@@ -23,18 +47,93 @@ LEARNING_RATES = [0.001, 0.0001, 0.00001]
 # %%
 
 
-class ResNet50(torchvision.models.resnet.ResNet):
-    def __init__(self, num_classes: int = 1000, dropout: float = 0.5):
-        super().__init__(torchvision.models.resnet.Bottleneck, [3, 4, 6, 3])
+class ResNet50(pl.LightningModule):
+    """Adaptation of the ResNet50 PyTorch model for audio data."""
+
+    def __init__(self, num_classes: int = 1000, dropout: float = 0.0):
+        """Initialize the ResNet50 model.
+
+        Args:
+            num_classes (int, optional): Number of classes. Defaults to 1000.
+            dropout (float, optional): Dropout rate. Defaults to 0.0.
+        """
+        super().__init__()
+        # torchvision.models.resnet.ResNet
+        self.model = torchvision.models.resnet.ResNet(
+            torchvision.models.resnet.Bottleneck, [3, 4, 6, 3]
+        )
         # remove stride 2 in the first 7x7 conv
-        self.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=1, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.model.conv1 = nn.Conv2d(
+            1, 64, kernel_size=7, stride=1, padding=3, bias=False
+        )
+        self.model.bn1 = nn.BatchNorm2d(64)
         # change avgpool size to 6x4
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 4))
+        self.model.avgpool = nn.AdaptiveAvgPool2d((6, 4))
         # adapt fc layer
-        self.fc = nn.Linear(49152, num_classes)
+        self.model.dropout = nn.Dropout(p=dropout)
+        self.model.fc = nn.Linear(49152, num_classes)
         # sigmoid
-        self.sigmoid = nn.Sigmoid()
+        # self.sigmoid = nn.Sigmoid()
+        # Initialize training metrics
+        self.train_acc = MultilabelAccuracy(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.train_auroc = MultilabelAUROC(
+            num_labels=self.num_classes, average="weighted"
+        )
+        self.train_f1 = MultilabelF1Score(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.train_hamming = MultilabelHammingDistance(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.train_precision = MultilabelPrecision(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.train_recall = MultilabelRecall(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.train_roc = MultilabelROC(num_labels=self.num_classes)
+        # Initialize validation metrics
+        self.val_acc = MultilabelAccuracy(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.val_auroc = MultilabelAUROC(
+            num_labels=self.num_classes, average="weighted"
+        )
+        self.val_f1 = MultilabelF1Score(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.val_hamming = MultilabelHammingDistance(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.val_precision = MultilabelPrecision(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.val_recall = MultilabelRecall(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.val_roc = MultilabelROC(num_labels=self.num_classes)
+        # Initialize test metrics
+        self.test_acc = MultilabelAccuracy(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.test_auroc = MultilabelAUROC(
+            num_labels=self.num_classes, average="weighted"
+        )
+        self.test_f1 = MultilabelF1Score(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.test_hamming = MultilabelHammingDistance(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.test_precision = MultilabelPrecision(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.test_recall = MultilabelRecall(
+            num_labels=self.num_classes, threshold=0.5, average="weighted"
+        )
+        self.test_roc = MultilabelROC(num_labels=self.num_classes)
 
     def forward(self, x):
         """Forward pass.
@@ -46,35 +145,268 @@ class ResNet50(torchvision.models.resnet.ResNet):
             torch.Tensor: output tensor
         """
         # same as torchvision.models.resnet.ResNet
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
 
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
 
-        x = self.avgpool(x)
+        x = self.model.avgpool(x)
+        x = self.model.dropout(x)  # Added dropout before classification layers
         x = torch.flatten(x, 1)
-        x = self.fc(x)
-        x = self.sigmoid(x)
+        x = self.model.fc(x)
+        # x = self.model.sigmoid(x)
 
         return x
 
+    def training_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        """Training step. Returns the loss value.
+
+        Args:
+            batch (torch.Tensor): batch of inputs
+            batch_idx (int): index of the batch
+
+        Returns:
+            torch.Tensor: loss value
+        """
+        inputs, targets = batch
+        outputs = self(inputs)
+        # Compute loss
+        loss = F.binary_cross_entropy_with_logits(outputs, targets)
+        # Log loss
+        self.log("train_loss", loss, on_step=False, on_epoch=True)
+        # Calculate metrics
+        self.train_acc(F.sigmoid(outputs), targets.int())
+        self.train_auroc(F.sigmoid(outputs), targets.int())
+        self.train_f1(F.sigmoid(outputs), targets.int())
+        self.train_hamming(F.sigmoid(outputs), targets.int())
+        self.train_precision(F.sigmoid(outputs), targets.int())
+        self.train_recall(F.sigmoid(outputs), targets.int())
+        # self.train_roc(F.sigmoid(outputs), targets.int())
+        # Log metrics
+        self.log("train_acc", self.train_acc, on_step=False, on_epoch=True)
+        self.log("train_auroc", self.train_auroc, on_step=False, on_epoch=True)
+        self.log("train_f1", self.train_f1, on_step=False, on_epoch=True)
+        self.log("train_hamming", self.train_hamming, on_step=False, on_epoch=True)
+        self.log("train_precision", self.train_precision, on_step=False, on_epoch=True)
+        self.log("train_recall", self.train_recall, on_step=False, on_epoch=True)
+        # self.log("train_roc", self.train_roc, on_step=False, on_epoch=True)
+        # log learning rate
+        self.log("lr", self.trainer.optimizers[0].param_groups[0]["lr"])
+        # Return loss value
+        return loss
+
+    def validation_step(
+        self, batch: torch.Tensor, batch_idx: int, dataloader_idx: int = 0
+    ) -> torch.Tensor:
+        """Validation step. Returns loss.
+
+        Args:
+            batch (torch.Tensor): Batch of images.
+            batch_idx (int): Batch index.
+
+        Returns:
+            torch.Tensor: Loss.
+        """
+        inputs, targets = batch
+        outputs = self(inputs)
+        # calculate loss
+        loss = F.binary_cross_entropy_with_logits(outputs, targets)
+        # log loss
+        self.log("val_loss", loss, on_step=False, on_epoch=True)
+        # calculate metrics
+        self.val_acc(F.sigmoid(outputs), targets.int())
+        self.val_auroc(F.sigmoid(outputs), targets.int())
+        self.val_f1(F.sigmoid(outputs), targets.int())
+        self.val_hamming(F.sigmoid(outputs), targets.int())
+        self.val_precision(F.sigmoid(outputs), targets.int())
+        self.val_recall(F.sigmoid(outputs), targets.int())
+        # self.val_roc(F.sigmoid(outputs), targets.int())
+        # log metrics
+        self.log("val_acc", self.val_acc, on_step=False, on_epoch=True)
+        self.log("val_auroc", self.val_auroc, on_step=False, on_epoch=True)
+        self.log("val_f1", self.val_f1, on_step=False, on_epoch=True)
+        self.log("val_hamming", self.val_hamming, on_step=False, on_epoch=True)
+        self.log("val_precision", self.val_precision, on_step=False, on_epoch=True)
+        self.log("val_recall", self.val_recall, on_step=False, on_epoch=True)
+        # self.log("val_roc", self.val_roc, on_step=False, on_epoch=True)
+        # return loss
+        return loss
+
+    def test_step(
+        self, batch: torch.Tensor, batch_idx: int, dataloader_idx: int = 0
+    ) -> torch.Tensor:
+        """Test step. Returns loss.
+
+        Args:
+            batch (torch.Tensor): Batch of images.
+            batch_idx (int): Batch index.
+
+        Returns:
+            torch.Tensor: Loss.
+        """
+        inputs, targets = batch
+        outputs = self(inputs)
+        # calculate loss
+        loss = F.binary_cross_entropy_with_logits(outputs, targets)
+        # log loss
+        self.log("test_loss", loss)
+        # calculate metrics
+        self.test_acc(F.sigmoid(outputs), targets.int())
+        self.test_auroc(F.sigmoid(outputs), targets.int())
+        self.test_f1(F.sigmoid(outputs), targets.int())
+        self.test_hamming(F.sigmoid(outputs), targets.int())
+        self.test_precision(F.sigmoid(outputs), targets.int())
+        self.test_recall(F.sigmoid(outputs), targets.int())
+        # self.test_roc(F.sigmoid(outputs), targets.int())
+        # log metrics
+        self.log("test_acc", self.test_acc)
+        self.log("test_auroc", self.test_auroc)
+        self.log("test_f1", self.test_f1)
+        self.log("test_hamming", self.test_hamming)
+        self.log("test_precision", self.test_precision)
+        self.log("test_recall", self.test_recall)
+        # self.log("test_roc", self.test_roc)
+        # return loss
+        return loss
+
+    def predict_step(
+        self, batch: torch.Tensor, batch_idx: int, dataloader_idx: int = 0
+    ) -> Any:
+        """Predict step. Returns predictions.
+
+        Args:
+            batch (torch.Tensor): Batch of images.
+            batch_idx (int): Batch index.
+
+        Returns:
+            Any: Predictions.
+        """
+        inputs, targets = batch
+        outputs = self(inputs)
+        return outputs
+
+    def configure_optimizers(self) -> Any:
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
+            lr=LEARNING_RATE,
+            betas=(0.9, 0.999),
+            weight_decay=WEIGHT_DECAY,
+        )
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            factor=0.1,
+            patience=config.EARLY_STOPPING_PATIENCE // 2,
+            threshold=0.0001,
+            threshold_mode="rel",
+            cooldown=0,
+            min_lr=0,
+            eps=1e-08,
+            verbose=True,
+        )
+        return [optimizer], [
+            {"scheduler": scheduler, "interval": "epoch", "monitor": "val_loss"}
+        ]
+
 
 # %% [markdown]
-# # Main
+# # Training and Testing
 
 # %%
 if __name__ == "__main__":
-    model = ResNet50()
-    print(model)
-    # generate a batch of 128 (96x64) inputs
-    batch_X = torch.randn(128, 1, 96, 64)
-    batch_y = model(batch_X)
-    print(batch_y.shape)
-
+    print("Starting training.")
+    # Split annotations into train and val sets
+    annotation = pd.read_csv(config.ANNOTATION_FILE)
+    train_annotation, val_annotation = train_test_split(
+        annotation, test_size=0.2, random_state=RANDOM_SEED
+    )
+    # Sort by Timestamp
+    train_annotation = train_annotation.sort_values(by="Timestamp")
+    val_annotation = val_annotation.sort_values(by="Timestamp")
+    # Save annotation files to csv
+    train_annotation.to_csv(config.TRAIN_ANNOTATION_FILE, index=False)
+    val_annotation.to_csv(config.VAL_ANNOTATION_FILE, index=False)
+    # Create model
+    # Search for checkpoint file in models directory
+    # file starts with experiment name
+    # file ends with .ckpt
+    checkpoint_file: str | None = None
+    for file in os.listdir(config.MODELS_DIRECTORY):
+        if file.startswith(EXPERIMENT_NAME) and file.endswith(".ckpt"):
+            # Checkpoint file found
+            # get file with highest val_auroc
+            if checkpoint_file is None:
+                checkpoint_file = file
+            elif file > checkpoint_file:
+                checkpoint_file = file
+    if checkpoint_file is not None:
+        # prepend models directory
+        checkpoint_file = os.path.join(config.MODELS_DIRECTORY, checkpoint_file)
+        model = ResNet50.load_from_checkpoint(
+            checkpoint_file, num_classes=config.NUM_CLASSES, dropout=0.5
+        )
+    else:
+        model = ResNet50(
+            num_classes=config.NUM_CLASSES,
+            dropout=0.5,
+        )
+    # Create datasets
+    train_dataset = AudioDataset(
+        config.TRAIN_ANNOTATION_FILE,
+        config.DATA_DIRECTORY,
+        transform=lambda x: x[None, :, :64],
+        target_transform=lambda x: x[0, :].float(),
+    )
+    val_dataset = AudioDataset(
+        config.VAL_ANNOTATION_FILE,
+        config.DATA_DIRECTORY,
+        transform=lambda x: x[None, :, :64],
+        target_transform=lambda x: x[0, :].float(),
+    )
+    # Create dataloaders
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=True,
+        num_workers=config.NUM_WORKERS,
+    )
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=False,
+        num_workers=config.NUM_WORKERS,
+    )
+    early_stopping = pl.callbacks.EarlyStopping(
+        monitor="val_loss", patience=config.EARLY_STOPPING_PATIENCE, mode="min"
+    )
+    loggers = [
+        pl.loggers.CSVLogger(config.LOG_DIRECTORY, name=EXPERIMENT_NAME),
+        pl.loggers.TensorBoardLogger(config.LOG_DIRECTORY, name=EXPERIMENT_NAME),
+    ]
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=config.MODELS_DIRECTORY,
+        filename=EXPERIMENT_NAME + "-{val_auroc:.2f}-{val_loss:.2f}-{epoch:02d}",
+        monitor="val_auroc",
+        verbose=True,
+        save_top_k=1,
+        save_weights_only=False,
+        mode="max",
+        auto_insert_metric_name=True,
+        every_n_epochs=1,
+        save_on_train_epoch_end=False,
+    )
+    trainer = pl.Trainer(
+        callbacks=[early_stopping, checkpoint_callback],
+        max_epochs=1000,
+        logger=loggers,
+        log_every_n_steps=min(50, len(train_dataloader)),
+    )
+    trainer.fit(model, train_dataloader, val_dataloader)
+    print("Finished training.")
 
 # %%
